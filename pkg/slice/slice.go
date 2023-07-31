7e843d94b4d2ce7af2640b7456a42b8df7e7b0bc
Viktor Nareiko <vnareiko.lt@gmail.com>
2023-07-31 23:09:46 +0000

    Removed unused collection method

--- pkg/slice/slice.go
+++ pkg/slice/slice.go
@@ -1,12 +1,5 @@
 package slice
 
-func Reverse[T comparable](ss []T) {
-	last := len(ss) - 1
-	for i := 0; i < len(ss)/2; i++ {
-		ss[i], ss[last-i] = ss[last-i], ss[i]
-	}
-}
-
 func Remove[T comparable](s []T, index int) []T {
 	return append(s[:index], s[index+1:]...)
 }
