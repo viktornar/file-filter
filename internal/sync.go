d7f42dc667b31a92cc89a701313d9ccec7055794
Viktor Nareiko <vnareiko.lt@gmail.com>
2023-08-01 17:04:38 +0000

    Adding bak extension when file is persisted in backup directory

--- internal/sync.go
+++ internal/sync.go
@@ -9,7 +9,8 @@ import (
 )
 
 const (
-	deletePrefix = "delete_"
+	deletePrefix    = "delete_"
+	backupExtension = ".bak"
 )
 
 func HandleWatcherEvent(event file.Event, destination string) {
@@ -32,27 +33,27 @@ func handleFileChange(event file.Event, destination string) {
 	// TODO: Complete logic when file is moved from one directory to another
 	switch event.Op {
 	case file.Create, file.Write:
-		file.CopyFile(event.Path, filepath.Join(destination, event.Name()))
+		file.CopyFile(event.Path, filepath.Join(destination, event.Name()+backupExtension))
 	case file.Rename:
-		parts := strings.Split(event.Path, "/")
+		_, newName := filepath.Split(event.Path)
+
 		oldName := event.Name()
-		newName := parts[len(parts)-1]
-		oldBackupPath := filepath.Join(destination, oldName)
-		newBackupPath := filepath.Join(destination, newName)
+		oldBackupPath := filepath.Join(destination, oldName+backupExtension)
+		newBackupPath := filepath.Join(destination, newName+backupExtension)
 
 		if strings.HasPrefix(newName, deletePrefix) {
 			file.DeleteFile(event.Path)
-			file.DeleteFile(filepath.Join(destination, oldName))
+			file.DeleteFile(filepath.Join(destination, oldName+backupExtension))
 			return
 		}
 
 		if _, err := os.Stat(oldBackupPath); errors.Is(err, os.ErrNotExist) {
-			file.CopyFile(event.Path, filepath.Join(destination, newName))
+			file.CopyFile(event.Path, filepath.Join(destination, newName+backupExtension))
 			return
 		}
 
 		file.RenameFile(oldBackupPath, newBackupPath)
 	case file.Remove:
-		file.DeleteFile(filepath.Join(destination, event.Name()))
+		file.DeleteFile(filepath.Join(destination, event.Name()+backupExtension))
 	}
 }
